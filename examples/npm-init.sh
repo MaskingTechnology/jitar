7e8970b48b921843e793193b84d5c663b3fb2a09
Bas Meeuwissen <bas@masking.tech>
2022-10-28 21:52:32 +0200

    Created script to install and build all examples

---
+++ examples/npm-init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# set -e
+
+# This script installs and builds all the examples.
+
+EXAMPLES_DIRS="1-basic 2-advanced 3-apps"
+
+echo "#"
+echo "# Updating the examples..."
+echo "#"
+
+npm_install_and_build()
+{
+    echo "# Install and build npm packages for $1"
+
+    cd $1
+    npm install
+    npm run build
+    cd ..
+}
+
+update_examples()
+{
+    cd $1
+
+    echo "# Updating $1"
+
+    for dir in ./*/
+    do
+        npm_install_and_build $dir
+    done
+
+    cd ..
+}
+
+for dir in $EXAMPLES_DIRS
+do
+    update_examples $dir
+done
