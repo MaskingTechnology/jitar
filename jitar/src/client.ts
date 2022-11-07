b1af72efe3fb690e92a1a2c1885f95b84b2ed4f6
Peter van Vliet <108156553+petermasking@users.noreply.github.com>
2022-11-07 17:01:30 +0100

    Vite plugin (#50)
    * First version of the Jitar -> Vite plugin
    * Added readme

--- jitar/src/client.ts
+++ jitar/src/client.ts
@@ -3,6 +3,9 @@ import LocalNode from './runtime/LocalNode.js';
 import RemoteGateway from './runtime/RemoteGateway.js';
 import RemoteRepository from './runtime/RemoteRepository.js';
 
+let client: LocalNode | undefined = undefined;
+const resolvers: ((value: LocalNode) => void)[] = [];
+
 export async function startClient(...segmentFiles: string[]): Promise<LocalNode>
 {
     const remoteUrl = document.location.origin;
@@ -14,5 +17,22 @@ export async function startClient(...segmentFiles: string[]): Promise<LocalNode>
     await node.setGateway(gateway);
     await node.setRepository(repository, segmentFiles);
 
+    client = node;
+
+    resolvers.forEach((resolve) => resolve(node));
+
     return node;
+}
+
+export async function getClient(): Promise<LocalNode>
+{
+    if (client === undefined)
+    {
+        return new Promise((resolve) =>
+        {
+            resolvers.push(resolve);
+        });
+    }
+
+    return client;
 }
