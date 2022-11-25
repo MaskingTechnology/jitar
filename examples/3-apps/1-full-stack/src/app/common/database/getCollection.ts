d36e8a9fe9217128dc36dd87381bf47d04c5f10a
Bas Meeuwissen <bas@masking.tech>
2022-11-25 20:37:39 +0100

    Updated tsconfig

--- examples/3-apps/1-full-stack/src/app/common/database/getCollection.ts
+++ examples/3-apps/1-full-stack/src/app/common/database/getCollection.ts
@@ -3,7 +3,7 @@ import { Collection } from 'mongodb';
 
 import getDatabase from './getDatabase.js';
 
-export default async function getCollection<T>(name: string): Promise<Collection<T>>
+export default async function getCollection<T>(name: string): Promise<Collection<T extends Document ? any: any>>
 {
     const database = await getDatabase();
 
