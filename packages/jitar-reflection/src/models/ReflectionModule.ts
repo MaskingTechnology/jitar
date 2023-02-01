c293b97da0a0aa1c4022b3c5cfad12e6d3ba3987
Peter van Vliet <peter@masking.tech>
2023-02-01 13:41:04 +0100

    #115: Added getting exported module members

--- packages/jitar-reflection/src/models/ReflectionModule.ts
+++ packages/jitar-reflection/src/models/ReflectionModule.ts
@@ -18,11 +18,20 @@ export default class ReflectionModule
 
     get members(): ReflectionMember[] { return this.#members; }
 
+    get exports(): ReflectionExport[] { return this.#exports; }
+
     get classes(): ReflectionMember[] { return this.#members.filter(member => member instanceof ReflectionClass); }
 
     get functions(): ReflectionMember[] { return this.#members.filter(member => member instanceof ReflectionFunction); }
 
     get fields(): ReflectionMember[] { return this.#members.filter(member => member instanceof ReflectionField); }
+
+    get exported(): ReflectionMember[]
+    {
+        const members = this.#exports.map(exported => this.getMember(exported.name));
+
+        return members.filter(member => member !== undefined) as ReflectionMember[];
+    }
 
     getMember(name: string): ReflectionMember | undefined
     {
