80104bec69e39337adbfde6245fe65b75dd58c51
Peter van Vliet <peter@masking.tech>
2023-02-18 00:07:23 +0100

    #115: Implemented basic reflection class tests

--- packages/jitar-reflection/test/models/ReflectionClass.spec.ts
+++ packages/jitar-reflection/test/models/ReflectionClass.spec.ts
@@ -1,123 +1,141 @@
 
+import ReflectionField from '../../src/models/ReflectionField';
+import ReflectionFunction from '../../src/models/ReflectionFunction';
+import ReflectionGetter from '../../src/models/ReflectionGetter';
+import ReflectionSetter from '../../src/models/ReflectionSetter';
+
+import { reflectionClass } from '../_fixtures/models/ReflectionClass.fixture';
+
 describe('models/ReflectionClass', () =>
 {
     describe('.fields', () =>
     {
-        it('should ...', () =>
+        it('should filter field members', () =>
         {
-            
-        });
-    });
+            const fields = reflectionClass.fields;
+            expect(fields.length).toBe(4);
 
-    describe('.functions', () =>
-    {
-        it('should ...', () =>
-        {
-            
+            expect(fields[0]).toBeInstanceOf(ReflectionField);
+            expect(fields[0].name).toBe('name');
+
+            expect(fields[1]).toBeInstanceOf(ReflectionField);
+            expect(fields[1].name).toBe('age');
+
+            expect(fields[2]).toBeInstanceOf(ReflectionField);
+            expect(fields[2].name).toBe('length');
+
+            expect(fields[3]).toBeInstanceOf(ReflectionField);
+            expect(fields[3].name).toBe('secret');
         });
     });
 
     describe('.getters', () =>
     {
-        it('should ...', () =>
+        it('should filter getter members', () =>
         {
-            
+            const getters = reflectionClass.getters;
+            expect(getters.length).toBe(2);
+
+            expect(getters[0]).toBeInstanceOf(ReflectionGetter);
+            expect(getters[0].name).toBe('name');
+
+            expect(getters[1]).toBeInstanceOf(ReflectionGetter);
+            expect(getters[1].name).toBe('age');
         });
     });
 
     describe('.setters', () =>
     {
-        it('should ...', () =>
+        it('should filter setter members', () =>
         {
-            
+            const setters = reflectionClass.setters;
+            expect(setters.length).toBe(1);
+
+            expect(setters[0]).toBeInstanceOf(ReflectionSetter);
+            expect(setters[0].name).toBe('age');
         });
     });
 
-    describe('.getField(name)', () =>
+    describe('.functions', () =>
     {
-        it('should ...', () =>
+        it('should filter function members', () =>
         {
-            
-        });
-    });
+            const functions = reflectionClass.functions;
+            expect(functions.length).toBe(3);
 
-    describe('.getFunction(name)', () =>
-    {
-        it('should ...', () =>
-        {
-            
-        });
-    });
+            expect(functions[0]).toBeInstanceOf(ReflectionFunction);
+            expect(functions[0].name).toBe('constructor');
 
-    describe('.getGetter(name)', () =>
-    {
-        it('should ...', () =>
-        {
-            
-        });
-    });
+            expect(functions[1]).toBeInstanceOf(ReflectionFunction);
+            expect(functions[1].name).toBe('secretStuff');
 
-    describe('.getSetter(name)', () =>
-    {
-        it('should ...', () =>
-        {
-            
-        });
-    });
-
-    describe('.hasField(name)', () =>
-    {
-        it('should ...', () =>
-        {
-            
-        });
-    });
-
-    describe('.hasFunction(name)', () =>
-    {
-        it('should ...', () =>
-        {
-            
-        });
-    });
-
-    describe('.hasGetter(name)', () =>
-    {
-        it('should ...', () =>
-        {
-            
-        });
-    });
-
-    describe('.hasSetter(name)', () =>
-    {
-        it('should ...', () =>
-        {
-            
+            expect(functions[2]).toBeInstanceOf(ReflectionFunction);
+            expect(functions[2].name).toBe('toString');
         });
     });
 
     describe('.canRead(name)', () =>
     {
-        it('should ...', () =>
+        it('should read a public field', () =>
         {
-            
+            const canRead = reflectionClass.canRead('length');
+
+            expect(canRead).toBe(true);
+        });
+
+        it('should read a private field with getter', () =>
+        {
+            const canRead = reflectionClass.canRead('name');
+
+            expect(canRead).toBe(true);
+        });
+
+        it('should not read a private field without a getter', () =>
+        {
+            const canRead = reflectionClass.canRead('secret');
+
+            expect(canRead).toBe(false);
         });
     });
 
     describe('.canWrite(name)', () =>
     {
-        it('should ...', () =>
+        it('should write a public field', () =>
         {
-            
+            const canWrite = reflectionClass.canWrite('length');
+
+            expect(canWrite).toBe(true);
+        });
+
+        it('should write a private field with setter', () =>
+        {
+            const canWrite = reflectionClass.canWrite('age');
+
+            expect(canWrite).toBe(true);
+        });
+
+        it('should not wite a private field without a setter', () =>
+        {
+            const canWrite = reflectionClass.canWrite('secret');
+
+            expect(canWrite).toBe(false);
         });
     });
 
     describe('.canCall(name)', () =>
     {
-        it('should ...', () =>
+        it('should call a public function', () =>
         {
-            
+            const canCall = reflectionClass.canCall('toString');
+
+            expect(canCall).toBe(true);
+        });
+
+        it('should not call a private function', () =>
+        {
+            const canCall = reflectionClass.canCall('secretStuff');
+
+            expect(canCall).toBe(false);
         });
     });
 });
