c34ea95ed9709b46e017b0b5ab7f76fcf00465c2
Peter van Vliet <peter@masking.tech>
2023-03-01 16:02:24 +0100

    #154: Added lib definition

---
+++ packages/jitar-serialization/src/lib.ts
@@ -0,0 +1,12 @@
+
+// Interfaces
+export { default as ClassLoader } from './interfaces/ClassLoader.js';
+
+// Types
+export { default as Loadable } from './types/Loadable.js';
+export { default as Serialized } from './types/Serialized.js';
+
+// Root
+export { default as Serializer } from './Serializer.js';
+export { default as SerializerBuilder } from './SerializerBuilder.js';
+export { default as ValueSerializer } from './ValueSerializer.js';
