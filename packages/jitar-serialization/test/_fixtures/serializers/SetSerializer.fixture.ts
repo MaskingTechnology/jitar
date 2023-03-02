b41bd5b8bd80b901ca1950c3a1f5a436c5f0981c
Peter van Vliet <peter@masking.tech>
2023-03-02 11:53:50 +0100

    #154: Implemented SetSerializer tests

--- packages/jitar-serialization/test/_fixtures/serializers/SetSerializer.fixture.ts
+++ packages/jitar-serialization/test/_fixtures/serializers/SetSerializer.fixture.ts
@@ -1,4 +1,12 @@
 
+import Serializer from "../../../src/Serializer";
+import PrimitiveSerializer from "../../../src/serializers/PrimitiveSerializer";
+import SetSerializer from "../../../src/serializers/SetSerializer";
+
+const parent = new Serializer();
+parent.addSerializer(new SetSerializer());
+parent.addSerializer(new PrimitiveSerializer());
+
 const emptySet: Set<unknown> = new Set();
 const mixedSet: Set<unknown> = new Set().add(1).add(true);
 const nestedSet: Set<unknown> = new Set().add('hello').add(new Set().add(false));
@@ -7,7 +15,15 @@ const serializedEmptySet = { serialized: true, name: 'Set', values: [] };
 const serializedMixedSet = { serialized: true, name: 'Set', values: [1, true] };
 const serializedNestedSet = { serialized: true, name: 'Set', values: ['hello', { serialized: true, name: 'Set', values: [false] }] };
 
+const nonObject = 42;
+const nonSet = new Map();
+const notSerialized = { name: 'Set', type: 'Set', bytes: [] };
+const wrongName = { serialized: true, name: 'Map', values: [] };
+const wrongValues = { serialized: true, name: 'Set', values: {} };
+
 export {
+    parent,
     emptySet, mixedSet, nestedSet,
-    serializedEmptySet, serializedMixedSet, serializedNestedSet
+    serializedEmptySet, serializedMixedSet, serializedNestedSet,
+    nonObject, nonSet, notSerialized, wrongName, wrongValues
 }
--- packages/jitar-serialization/test/serializers/SetSerializer.spec.ts
+++ packages/jitar-serialization/test/serializers/SetSerializer.spec.ts
@@ -4,6 +4,84 @@ import { describe, expect, it } from 'vitest';
 import SetSerializer from '../../src/serializers/SetSerializer';
 
 import {
+    parent,
     emptySet, mixedSet, nestedSet,
-    serializedEmptySet, serializedMixedSet, serializedNestedSet
+    serializedEmptySet, serializedMixedSet, serializedNestedSet,
+    nonObject, nonSet, notSerialized, wrongName, wrongValues
 } from '../_fixtures/serializers/SetSerializer.fixture';
+
+const serializer = new SetSerializer();
+serializer.parent = parent;
+
+describe('serializers/SetSerializer', () =>
+{
+    describe('.canSerialize(value)', () =>
+    {
+        it('should tell it can serialize a set', () =>
+        {
+            const supportsSet = serializer.canSerialize(emptySet);
+
+            expect(supportsSet).toBe(true);
+        });
+
+        it('should tell it can not serialize others', () =>
+        {
+            const supportsNonObject = serializer.canSerialize(nonObject);
+            const supportsNonSet = serializer.canSerialize(nonSet);
+
+            expect(supportsNonObject).toBe(false);
+            expect(supportsNonSet).toBe(false);
+        });
+    });
+
+    describe('.canDeserialize(value)', () =>
+    {
+        it('should tell it can deserialize a set', () =>
+        {
+            const supportsSet = serializer.canDeserialize(serializedEmptySet);
+
+            expect(supportsSet).toBe(true);
+        });
+
+        it('should tell it can not deserialize others', () =>
+        {
+            const supportsNonObject = serializer.canDeserialize(nonObject);
+            const supportsNotSerialized = serializer.canDeserialize(notSerialized);
+            const supportsWrongName = serializer.canDeserialize(wrongName);
+            const supportsWrongValues = serializer.canDeserialize(wrongValues);
+
+            expect(supportsNonObject).toBe(false);
+            expect(supportsNotSerialized).toBe(false);
+            expect(supportsWrongName).toBe(false);
+            expect(supportsWrongValues).toBe(false);
+        });
+    });
+
+    describe('.serialize(data)', () =>
+    {
+        it('should serialize a set', async () =>
+        {
+            const resultEmptySet = await serializer.serialize(emptySet);
+            const resultMixedSet = await serializer.serialize(mixedSet);
+            const resultNestedSet = await serializer.serialize(nestedSet);
+
+            expect(resultEmptySet).toEqual(serializedEmptySet);
+            expect(resultMixedSet).toEqual(serializedMixedSet);
+            expect(resultNestedSet).toEqual(serializedNestedSet);
+        });
+    });
+
+    describe('.deserialize(object)', () =>
+    {
+        it('should deserialize a set', async () =>
+        {
+            const resultEmptySet = await serializer.deserialize(serializedEmptySet);
+            const resultMixedSet = await serializer.deserialize(serializedMixedSet);
+            const resultNestedSet = await serializer.deserialize(serializedNestedSet);
+
+            expect(resultEmptySet).toEqual(emptySet);
+            expect(resultMixedSet).toEqual(mixedSet);
+            expect(resultNestedSet).toEqual(nestedSet);
+        });
+    });
+});
