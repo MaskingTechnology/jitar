c6af77a92c3fc5e593fc10e6bd18352faae727f6
Bas Meeuwissen <bas@masking.tech>
2023-03-08 11:41:19 +0100

    Cleaned up a bit

--- examples/4-integrations/4-lit/src/my-element.ts
+++ examples/4-integrations/4-lit/src/my-element.ts
@@ -52,54 +52,9 @@ export class MyElement extends LitElement
       filter: drop-shadow(0 0 2em #325cffaa);
     }
 
-    .card {
-      padding: 2em;
-    }
-
-    .read-the-docs {
-      color: #888;
-    }
-
     h1 {
       font-size: 3.2em;
       line-height: 1.1;
-    }
-
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-    a:hover {
-      color: #535bf2;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
-    }
-
-    @media (prefers-color-scheme: light) {
-      a:hover {
-        color: #747bff;
-      }
-      button {
-        background-color: #f9f9f9;
-      }
     }
   `
 }
--- examples/4-integrations/4-lit/types/shared/sayHello.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function sayHello(name: string): Promise<string>;
