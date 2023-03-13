dea6a8f8b46c77f3b17a555b5729a36c8a06badf
Bas Meeuwissen <bas@masking.tech>
2023-03-13 09:24:56 +0100

    Built from vanilla-ts vite template

--- examples/4-integrations/5-solid/src/App.tsx
+++ examples/4-integrations/5-solid/src/App.tsx
@@ -1,10 +1,25 @@
+
+import solidjslogo from './assets/solid.svg';
 import type { Component } from 'solid-js';
-import Comp from './Comp';
+import { createResource } from 'solid-js';
+import { sayHello } from './shared/sayHello';
+
+const sayHelloResource = async () => {
+  return await sayHello('Vite + Solid + Jitar');
+}
 
 const App: Component = () => {
+  const [message] = createResource(sayHelloResource);
+
   return (
     <>
-      <Comp />
+        <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://solidjs.com" target="_blank">
+        <img src={solidjslogo} class="logo solid" alt="Solid logo" />
+        </a>
+        <h1>{ message }</h1>
     </>
   );
 };
---
+++ examples/4-integrations/5-solid/src/main.tsx
@@ -0,0 +1,8 @@
+
+import { render } from 'solid-js/web';
+
+import App from './App';
+
+const root = document.getElementById('app');
+
+render(() => <App />, root!);
