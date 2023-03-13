27a22df206957f51b90e99d6363ac55df10e576c
Peter van Vliet <peter@masking.tech>
2023-03-13 16:42:30 +0100

    #155: Added remote builders tests

---
+++ packages/jitar-caching/test/building/utils/RemoteBuilder.spec.ts
@@ -0,0 +1,25 @@
+
+import { describe, expect, it } from 'vitest';
+
+import RemoteBuilder from '../../../src/building/utils/RemoteBuilder';
+
+import
+{
+    segmentModule,
+    codeResult
+} from '../../_fixtures/building/utils/RemoteBuilder.fixture';
+
+const remoteBuilder = new RemoteBuilder();
+
+describe('runtime/utils/RemoteBuilder', () =>
+{
+    describe('.build(module)', () =>
+    {
+        it('should create remote calls for all procedure implementations', () =>
+        {
+            const result = remoteBuilder.build(segmentModule);
+
+            expect(result).toBe(codeResult);
+        });
+    });
+});
