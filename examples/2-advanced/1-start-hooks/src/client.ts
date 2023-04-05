ae282545edf2791a14678b386c8f0884d4bf5bc8
Peter van Vliet <peter@masking.tech>
2023-04-05 14:22:26 +0200

    #156: Updated the start hooks example to work with the new packages

--- examples/2-advanced/1-start-hooks/src/client.ts
+++ examples/2-advanced/1-start-hooks/src/client.ts
@@ -1,12 +1,11 @@
 
-// @ts-ignore (the import will be valid at runtime)
-import { startClient } from '/jitar/client.js';
+import { startClient } from 'jitar-runtime';
 
 const client = await startClient('client');
 
 // We need to import the sayHello component with the Jitar client.
 // This enables the segmentation of the application.
-const { default: sayBoth } = await client.import('./greetings/sayBoth.js');
+const { default: sayBoth } = await client.import('./greetings/sayBoth.js') as any;
 
 const message = await sayBoth('John', 'Doe');
 
