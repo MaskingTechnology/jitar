2d1e56295c77644be5590a8f68c1557cc8fafe62
Peter van Vliet <peter@masking.tech>
2023-04-12 15:36:41 +0200

    #242: Solved linting issue

--- packages/runtime/test/_fixtures/utils/ModuleLoader.fixture.ts
+++ packages/runtime/test/_fixtures/utils/ModuleLoader.fixture.ts
@@ -3,6 +3,7 @@ const moduleImporter = async (specifier: string) =>
 {
     if (specifier === '/root/app/public/app.js')
     {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
         return { default: function app() {} };
     }
 
