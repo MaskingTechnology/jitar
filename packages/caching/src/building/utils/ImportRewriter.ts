b5ef14766cebb44bdc5ba5705313c68087a5ac6a
Peter van Vliet <peter@masking.tech>
2023-04-17 14:08:32 +0200

    #213: added automatic .js appending to the caching

--- packages/caching/src/building/utils/ImportRewriter.ts
+++ packages/caching/src/building/utils/ImportRewriter.ts
@@ -10,20 +10,25 @@ const reflector = new Reflector();
 
 export default class ImportRewriter
 {
-    rewrite(code: string): string
+    rewrite(code: string, includeSystem: boolean): string
     {
-        const replacer = (statement: string) => this.#replaceImport(statement);
+        const replacer = (statement: string) => this.#replaceImport(statement, includeSystem);
 
         return code.replaceAll(IMPORT_PATTERN, replacer);
     }
 
-    #replaceImport(statement: string): string
+    #replaceImport(statement: string, includeSystem: boolean): string
     {
         const dependency = reflector.parseImport(statement);
 
-        return this.#isSystemDependency(dependency)
-            ? this.#rewriteImport(dependency)
-            : statement;
+        if (this.#isSystemDependency(dependency))
+        {
+            return includeSystem
+                ? this.#rewriteSystemImport(dependency)
+                : statement;
+        }
+
+        return this.#rewriteApplicationImport(dependency);
     }
 
     #isSystemDependency(dependency: ReflectionImport): boolean
@@ -31,28 +36,50 @@ export default class ImportRewriter
         return NON_SYSTEM_INDICATORS.some(indicator => dependency.from.startsWith(indicator, 1)) === false;
     }
 
-    #rewriteImport(dependency: ReflectionImport): string
-    {
+    #rewriteSystemImport(dependency: ReflectionImport): string
+    {   
+        const from = this.#rewriteImportFrom(dependency);
+
         if (dependency.members.length === 0)
         {
-            return `await __getDependency(${dependency.from});`;
+            return `await __getDependency('${from}');`;
         }
 
-        const members = this.#rewriteImportMembers(dependency);
+        const members = this.#rewriteImportMembers(dependency, ':');
 
-        return `const ${members} = await __getDependency(${dependency.from});`;
+        return `const ${members} = await __getDependency('${from}');`;
     }
 
-    #rewriteImportMembers(dependency: ReflectionImport): string
+    #rewriteApplicationImport(dependency: ReflectionImport): string
+    {
+        const members = this.#rewriteImportMembers(dependency, 'as');
+        const from = this.#rewriteImportFrom(dependency);
+
+        return `import ${members} from '${from}';`;
+    }
+
+    #rewriteImportMembers(dependency: ReflectionImport, aliasSpecifier: string): string
     {
         if (this.#mustUseAs(dependency))
         {
             return dependency.members[0].as;
         }
 
-        const members = dependency.members.map(member => member.name !== member.as ? `${member.name}: ${member.as}` : member.name);
+        const members = dependency.members.map(member => member.name !== member.as ? `${member.name} ${aliasSpecifier} ${member.as}` : member.name);
 
         return `{ ${members.join(', ')} }`;
+    }
+
+    #rewriteImportFrom(dependency: ReflectionImport): string
+    {
+        const from = dependency.from.substring(1, dependency.from.length - 1);
+
+        if (NON_SYSTEM_INDICATORS.some(indicator => from.startsWith(indicator)))
+        {
+            return from.endsWith('.js') ? from : `${from}.js`;
+        }
+        
+        return from;
     }
 
     #mustUseAs(dependency: ReflectionImport): boolean
