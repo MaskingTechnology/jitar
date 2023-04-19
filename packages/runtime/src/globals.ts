48be7c98f3b24025c925515a727e27ef89e7e33a
Peter van Vliet <108156553+petermasking@users.noreply.github.com>
2023-04-19 11:07:44 +0200

    feat(packaging): Added global runtime types (#258)

--- packages/runtime/src/globals.ts
+++ packages/runtime/src/globals.ts
@@ -2,8 +2,17 @@
 import { getDependency, runProcedure } from './hooks.js';
 import ProcedureNotAccessible from './errors/ProcedureNotAccessible.js';
 
+declare global
+{
+    const __getDependency: typeof getDependency;
+    const __runProcedure: typeof runProcedure;
+}
+
 const globals = globalThis as Record<string, unknown>;
 
+// Available for external use
 globals.__getDependency = getDependency;
 globals.__runProcedure = runProcedure;
+
+// Internal use only
 globals.ProcedureNotAccessible = ProcedureNotAccessible;
