a1b6f1e19ee2b56a43ae9664dc6a58ebc6c70bd0
Bas Meeuwissen <bas@masking.tech>
2023-04-26 09:38:10 +0200

    feat(serialization): added the url serializer (#262)

--- packages/serialization/src/serializers/errors/InvalidDateString.ts
+++ packages/serialization/src/serializers/errors/InvalidDateString.ts
@@ -3,6 +3,6 @@ export default class InvalidDateString extends Error
 {
     constructor(dateString: string)
     {
-        super(`Invalid date string: '${dateString}'`);
+        super(`Invalid date string '${dateString}'`);
     }
 }
