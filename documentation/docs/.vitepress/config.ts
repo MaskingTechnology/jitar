81bd10d83b55951e2d95288e52057a05611bc6f7
Bas Meeuwissen <bas@masking.tech>
2023-05-17 06:31:36 +0200

    Added / before each location (enables highlight)

--- documentation/docs/.vitepress/config.ts
+++ documentation/docs/.vitepress/config.ts
@@ -16,103 +16,103 @@ export default defineConfig({
       {
         text: 'INTRODUCTION',
         items: [
-          { text: 'What is Jitar?', link: 'introduction/what-is-jitar' },
-          { text: 'Installation', link: 'introduction/installation' },
-          { text: 'Quick Start', link: 'introduction/quick-start' }
+          { text: 'What is Jitar?', link: '/introduction/what-is-jitar' },
+          { text: 'Installation', link: '/introduction/installation' },
+          { text: 'Quick Start', link: '/introduction/quick-start' }
         ]
       },
       {
         text: 'FUNDAMENTALS',
         items: [
-          { text: 'Overview', link: 'fundamentals/overview' },
-          { text: 'Runtime services', link: 'fundamentals/runtime-services' }
+          { text: 'Overview', link: '/fundamentals/overview' },
+          { text: 'Runtime services', link: '/fundamentals/runtime-services' }
         ]
       },
       {
         text: 'DEVELOP',
         items: [
-          { text: 'Application structure', link: 'develop/application-structure' },
-          { text: 'Writing functions', link: 'develop/writing-functions' },
-          { text: 'Data sharing', link: 'develop/data-sharing' },
-          { text: 'Error handling', link: 'develop/error-handling' },
-          { text: 'State management', link: 'develop/state-management' },
-          { text: 'Data consistency', link: 'develop/data-consistency' },
-          { text: 'Middleware', link: 'develop/middleware' },
-          { text: 'Validation', link: 'develop/validation' },
-          { text: 'Security', link: 'develop/security' },
-          { text: 'Assets', link: 'develop/assets' },
-          { text: 'Debugging', link: 'develop/debugging' },
-          { text: 'Integration', link: 'develop/integration' }
+          { text: 'Application structure', link: '/develop/application-structure' },
+          { text: 'Writing functions', link: '/develop/writing-functions' },
+          { text: 'Data sharing', link: '/develop/data-sharing' },
+          { text: 'Error handling', link: '/develop/error-handling' },
+          { text: 'State management', link: '/develop/state-management' },
+          { text: 'Data consistency', link: '/develop/data-consistency' },
+          { text: 'Middleware', link: '/develop/middleware' },
+          { text: 'Validation', link: '/develop/validation' },
+          { text: 'Security', link: '/develop/security' },
+          { text: 'Assets', link: '/develop/assets' },
+          { text: 'Debugging', link: '/develop/debugging' },
+          { text: 'Integration', link: '/develop/integration' }
         ]
       },
       {
         text: 'DEPLOY',
         items: [
-          { text: 'Segmentation', link: 'deploy/segmentation' },
-          { text: 'Customization', link: 'deploy/customization' },
-          { text: 'Environments', link: 'deploy/environments' },
-          { text: 'Load balancing', link: 'deploy/load-balancing' },
-          { text: 'Health checks', link: 'deploy/health-checks' },
-          { text: 'CORS', link: 'deploy/cors' }
+          { text: 'Segmentation', link: '/deploy/segmentation' },
+          { text: 'Customization', link: '/deploy/customization' },
+          { text: 'Environments', link: '/deploy/environments' },
+          { text: 'Load balancing', link: '/deploy/load-balancing' },
+          { text: 'Health checks', link: '/deploy/health-checks' },
+          { text: 'CORS', link: '/deploy/cors' }
         ]
       },
       {
         text: 'MONITOR',
         items: [
-          { text: 'Logging', link: 'monitor/logging' },
-          { text: 'Health', link: 'monitor/health' },
-          { text: 'Nodes', link: 'monitor/nodes' },
-          { text: 'Procedures', link: 'monitor/procedures' }
+          { text: 'Logging', link: '/monitor/logging' },
+          { text: 'Health', link: '/monitor/health' },
+          { text: 'Nodes', link: '/monitor/nodes' },
+          { text: 'Procedures', link: '/monitor/procedures' }
         ]
       },
       {
         text: 'EXAMPLES',
         items: [
-          { text: 'Concepts', link: 'examples/concepts' },
-          { text: 'Apps', link: 'examples/apps' }
+          { text: 'Concepts', link: '/examples/concepts' },
+          { text: 'Apps', link: '/examples/apps' }
         ]
       },
       {
         text: 'TUTORIALS',
         items: [
-          { text: 'Build a full-stack app', link: 'tutorials/build-a-full-stack-app' },
-          { text: 'Build for scale', link: 'tutorials/build-for-scale' },
-          { text: 'Integration with other apps', link: 'tutorials/integration-with-other-apps' },
-          { text: 'Dynamically switch version', link: 'tutorials/dynamically-switch-version' },
-          { text: 'Creating a cluster', link: 'tutorials/creating-a-cluster' },
-          { text: 'Add Jitar to an existing project', link: 'tutorials/add-jitar-to-an-existing-project' },
-          { text: 'Migrate away from Jitar', link: 'tutorials/migrate-away-from-jitar' }
+          { text: 'Build a full-stack app', link: '/tutorials/build-a-full-stack-app' },
+          { text: 'Build for scale', link: '/tutorials/build-for-scale' },
+          { text: 'Integration with other apps', link: '/tutorials/integration-with-other-apps' },
+          { text: 'Dynamically switch version', link: '/tutorials/dynamically-switch-version' },
+          { text: 'Creating a cluster', link: '/tutorials/creating-a-cluster' },
+          { text: 'Add Jitar to an existing project', link: '/tutorials/add-jitar-to-an-existing-project' },
+          { text: 'Migrate away from Jitar', link: '/tutorials/migrate-away-from-jitar' }
         ]
       },
       {
         text: 'LEFTOVERS',
         items: [
-          { text: 'REST API\s', link: 'leftovers/rest-apis' },
-          { text: 'Caching', link: 'leftovers/caching' }
+          { text: 'REST API\s', link: '/leftovers/rest-apis' },
+          { text: 'Caching', link: '/leftovers/caching' }
         ]
       },
       {
         text: 'COMPARIZATIONS',
         items: [
-          { text: 'Jitar vs. Meteor', link: 'comparizations/jitar-vs-meteor' },
-          { text: 'Jitar vs. tRPC', link: 'comparizations/jitar-vs-trpc' },
-          { text: 'Jitar vs. Service Weaver', link: 'comparizations/jitar-vs-service-weaver' },
-          { text: 'Jitar vs. Next.js', link: 'comparizations/jitar-vs-nextjs' }
+          { text: 'Jitar vs. Meteor', link: '/comparizations/jitar-vs-meteor' },
+          { text: 'Jitar vs. tRPC', link: '/comparizations/jitar-vs-trpc' },
+          { text: 'Jitar vs. Service Weaver', link: '/comparizations/jitar-vs-service-weaver' },
+          { text: 'Jitar vs. Next.js', link: '/comparizations/jitar-vs-nextjs' }
         ]
       },
       {
         text: 'COMMUNITY',
         items: [
-          { text: 'Get help', link: 'community/get-help' },
-          { text: 'Give feedback', link: 'community/give-feedback' },
-          { text: 'Contribute', link: 'community/contribute' }
+          { text: 'Get help', link: '/community/get-help' },
+          { text: 'Give feedback', link: '/community/give-feedback' },
+          { text: 'Contribute', link: '/community/contribute' }
         ]
       },
       {
         text: 'ABOUT',
         items: [
-          { text: 'Our goal', link: 'about/our-goal' },
-          { text: 'Our team', link: 'about/our-team' }
+          { text: 'Our goal', link: '/about/our-goal' },
+          { text: 'Our team', link: '/about/our-team' }
         ]
       }
     ],
@@ -122,8 +122,8 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/MaskingTechnology/jitar' },
-      { icon: 'discord', link: 'https://discord.gg/Bqwy8azp5R' }
+      { icon: 'github', link: '/https://github.com/MaskingTechnology/jitar' },
+      { icon: 'discord', link: '/https://discord.gg/Bqwy8azp5R' }
     ],
 
     footer: {
