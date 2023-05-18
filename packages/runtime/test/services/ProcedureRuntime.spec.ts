5425488dc88f91caa3a4832fd48567674cc4df7c
Peter van Vliet <peter@masking.tech>
2023-05-18 16:06:05 +0200

    #290: Reversed the execution order of the middleware

--- packages/runtime/test/services/ProcedureRuntime.spec.ts
+++ packages/runtime/test/services/ProcedureRuntime.spec.ts
@@ -22,7 +22,7 @@ describe('services/ProcedureRuntime', () =>
             expect(headers.get('first')).toBe('yes');
             expect(headers.get('second')).toBe('yes');
             expect(headers.get('third')).toBe('yes');
-            expect(headers.get('last')).toBe('1'); // The last middleware to be called is the first one to be added
+            expect(headers.get('last')).toBe('3'); // The last middleware to be called is the last one added
         });
     });
 });
