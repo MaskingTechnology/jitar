d40064fe4f8ef9004451740cc2ce57864c2e3a5c
Bas Meeuwissen <bas@masking.tech>
2023-05-31 09:31:10 +0200

    Removed the last updated datetime

--- documentation/docs/.vitepress/config.ts
+++ documentation/docs/.vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  lastUpdated: true,
   lang: 'en-US',
   title: "Jitar",
   description: "Documentation",
