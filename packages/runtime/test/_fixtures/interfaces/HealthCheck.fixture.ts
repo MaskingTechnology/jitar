b58bc844a37d848767369e6dd490b3d44da90e7c
Bas Meeuwissen <bas@masking.tech>
2023-06-08 14:47:27 +0200

    feat(runtime): added timeout option to health checks (#316)

--- packages/runtime/test/_fixtures/interfaces/HealthCheck.fixture.ts
+++ packages/runtime/test/_fixtures/interfaces/HealthCheck.fixture.ts
@@ -3,24 +3,58 @@ import HealthCheck from '../../../src/interfaces/HealthCheck';
 
 class HealthyCheck implements HealthCheck
 {
+    get timeout() { return undefined; }
+
     async isHealthy(): Promise<boolean> { return true; }
 }
 
 class UnhealthyCheck implements HealthCheck
 {
+    get timeout() { return undefined; }
+
     async isHealthy(): Promise<boolean> { return false; }
 }
 
 class ErrorHealthCheck implements HealthCheck
 {
+    get timeout() { return undefined; }
+
     async isHealthy(): Promise<boolean> { throw new Error('ErrorHealthCheck'); }
+}
+
+class TimedOutHealthCheck implements HealthCheck
+{
+    get timeout() { return 50; }
+
+    async isHealthy(): Promise<boolean>
+    {
+        return new Promise((resolve) =>
+        {
+            setTimeout(resolve, 5000);
+        }).then(() => true);
+    }
+}
+
+class InTimeHealthCheck implements HealthCheck
+{
+    get timeout() { return 5000; }
+
+    async isHealthy(): Promise<boolean>
+    {
+        return new Promise((resolve) =>
+        {
+            setTimeout(resolve, 50);
+        }).then(() => true);
+    }
 }
 
 const HEALTH_CHECKS =
 {
     GOOD: new HealthyCheck(),
     BAD: new UnhealthyCheck(),
-    ERROR: new ErrorHealthCheck()
+    ERROR: new ErrorHealthCheck(),
+    TIMEDOUT: new TimedOutHealthCheck(),
+    INTIME: new InTimeHealthCheck()
 };
 
 export { HEALTH_CHECKS };
--- packages/runtime/test/services/Runtime.spec.ts
+++ packages/runtime/test/services/Runtime.spec.ts
@@ -6,28 +6,44 @@ import { RUNTIMES } from '../_fixtures/services/Runtime.fixture';
 const goodRuntime = RUNTIMES.GOOD;
 const badRuntime = RUNTIMES.BAD;
 const errorRuntime = RUNTIMES.ERROR;
+const timedOutRuntime = RUNTIMES.TIMEDOUT;
+const inTimeRuntime = RUNTIMES.INTIME;
 
 describe('services/Runtime', () =>
 {
     describe('.isHealthy()', () =>
     {
-        it('is unhealth with exception', async () =>
+        it('should be unhealthy when an error occurs', async () =>
         {
             const isHealthy = await errorRuntime.isHealthy();
 
             expect(isHealthy).toBeFalsy();
         });
 
-        it('is unhealth with bad health check', async () =>
+        it('should be unhealthy with a bad health check', async () =>
         {
             const isHealthy = await badRuntime.isHealthy();
 
             expect(isHealthy).toBeFalsy();
         });
 
-        it('is healthy with good health check', async () =>
+        it('should be healthy with a good health check', async () =>
         {
             const isHealthy = await goodRuntime.isHealthy();
+
+            expect(isHealthy).toBeTruthy();
+        });
+
+        it('should be unhealthy with a timed out health check', async () =>
+        {
+            const isHealthy = await timedOutRuntime.isHealthy();
+
+            expect(isHealthy).toBeFalsy();
+        });
+
+        it('should be healthy with an in time health check', async () =>
+        {
+            const isHealthy = await inTimeRuntime.isHealthy();
 
             expect(isHealthy).toBeTruthy();
         });
@@ -35,7 +51,7 @@ describe('services/Runtime', () =>
 
     describe('.getHealth()', () =>
     {
-        it('is unhealth with exception', async () =>
+        it('should get a false state when an error occurs', async () =>
         {
             const health = await errorRuntime.getHealth();
             const result = health.get('error');
@@ -43,7 +59,7 @@ describe('services/Runtime', () =>
             expect(result).toBeFalsy();
         });
 
-        it('is unhealth with bad health check', async () =>
+        it('should get a false state with a bad health check', async () =>
         {
             const health = await badRuntime.getHealth();
             const result = health.get('bad');
@@ -51,10 +67,26 @@ describe('services/Runtime', () =>
             expect(result).toBeFalsy();
         });
 
-        it('is healthy with good health check', async () =>
+        it('should get a true state with a good health check', async () =>
         {
             const health = await goodRuntime.getHealth();
             const result = health.get('good');
+
+            expect(result).toBeTruthy();
+        });
+
+        it('should get a false state with a timed out health check', async () =>
+        {
+            const health = await timedOutRuntime.getHealth();
+            const result = health.get('timedOut');
+
+            expect(result).toBeFalsy();
+        });
+
+        it('should get a true state with an in time health check', async () =>
+        {
+            const health = await inTimeRuntime.getHealth();
+            const result = health.get('inTime');
 
             expect(result).toBeTruthy();
         });
