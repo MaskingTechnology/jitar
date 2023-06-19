19d9649c7133edd76013610f906e98ffb99afef5
Bas Meeuwissen <bas@masking.tech>
2023-06-19 16:33:54 +0200

    renamed the exported function for building the server

--- packages/server-nodejs/src/server.ts
+++ packages/server-nodejs/src/server.ts
@@ -3,13 +3,13 @@ import { ModuleImporter, ModuleLoader } from '@jitar/runtime';
 
 import JitarServer from './JitarServer.js';
 
-export async function startServer(moduleImporter: ModuleImporter): Promise<JitarServer>
+export async function buildServer(moduleImporter: ModuleImporter): Promise<JitarServer>
 {
     ModuleLoader.setImporter(moduleImporter);
 
     const server = new JitarServer();
 
-    await server.start();
+    await server.build();
 
     return server;
 }
