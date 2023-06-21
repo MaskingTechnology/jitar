dd49361631f4ead82fb2b044e15e50ab084fe615
Bas Meeuwissen <bas@masking.tech>
2023-06-21 09:21:56 +0200

    feat(serialization): regexp serialization support (#319)
    Co-authored-by: Peter van Vliet <108156553+petermasking@users.noreply.github.com>

---
+++ packages/serialization/test/_fixtures/serializers/RegExpSerializer.fixture.ts
@@ -0,0 +1,21 @@
+
+const validRegExp = new RegExp('w+', 'gi');
+
+const serializedValidRegExp = { serialized: true, name: 'RegExp', source: 'w+', flags: 'gi' };
+
+const nonObject = 42;
+const nonRegExp = new Map();
+const notSerialized = { name: 'RegExp', source: 'w+', flags: 'gi' };
+const invalidName = { serialized: true, name: 'Map', source: 'w+', flags: 'gi' };
+const invalidRegExpSource = { serialized: true, name: 'Date', source: true, flags: 'g' };
+const invalidRegExpFlag = { serialized: true, name: 'Date', source: 'w+', flags: true };
+const serializedInvalidRegExpSource = { serialized: true, name: 'Date', source: 'sel/\\', flags: 'g' };
+const serializedInvalidRegExpFlag = { serialized: true, name: 'Date', source: 'w+', flags: 'true' };
+
+export
+{
+    validRegExp,
+    serializedValidRegExp,
+    nonObject, nonRegExp, notSerialized, invalidName, invalidRegExpSource, invalidRegExpFlag,
+    serializedInvalidRegExpSource, serializedInvalidRegExpFlag
+};
