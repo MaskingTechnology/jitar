42e5fb99e165e0c988a97b2a3add596fdac1849e
Bas Meeuwissen <bas@masking.tech>
2023-06-21 16:28:13 +0200

    Update packages/serialization/src/serializers/errors/InvalidBigIntString.ts
    Co-authored-by: Peter van Vliet <108156553+petermasking@users.noreply.github.com>

--- packages/serialization/src/serializers/errors/InvalidBigIntString.ts
+++ packages/serialization/src/serializers/errors/InvalidBigIntString.ts
@@ -3,6 +3,6 @@ export default class InvalidBigIntString extends Error
 {
     constructor(bigIntString: string)
     {
-        super(`Invalid bigInt string '${bigIntString}'`);
+        super(`Invalid BigInt string '${bigIntString}'`);
     }
 }
