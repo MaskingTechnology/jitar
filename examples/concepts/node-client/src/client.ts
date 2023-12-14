0c9e2a284c6fb7a8ff7835361cd97cdfb405b7d0
Peter van Vliet <peter@masking.tech>
2023-12-14 21:50:08 +0100

    #398: Updated examples that use the Jitar client

--- examples/concepts/node-client/src/client.ts
+++ examples/concepts/node-client/src/client.ts
@@ -3,7 +3,7 @@ import { startClient } from 'jitar';
 
 const client = await startClient('http://127.0.0.1:3000', ['client']);
 
-const { default: getLuckyNumber } = await client.import('getLuckyNumber') as any;
+const { default: getLuckyNumber } = await client.import('getLuckyNumber', 'application') as any;
 
 const number = await getLuckyNumber(0, 100);
 
