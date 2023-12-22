cec72aef84f93826ed0d11fccb5b22f8431d653d
Bas Meeuwissen <bas@masking.tech>
2023-12-22 14:12:43 +0100

    #402: added validation for input string

--- packages/runtime/src/utils/VersionParser.ts
+++ packages/runtime/src/utils/VersionParser.ts
@@ -3,13 +3,25 @@ import InvalidVersionNumber from '../errors/InvalidVersionNumber.js';
 
 import Version from '../models/Version.js';
 
+const versionExpression = /^\d+(?:\.\d+){0,2}$/;
+
 export default class VersionParser
 {
-    static parse(number: string): Version
+    static parse(number: unknown): Version
     {
+        if (typeof number !== 'string')
+        {
+            throw new InvalidVersionNumber(String(number));
+        }
+
         if (number.trim().length === 0)
         {
             return Version.DEFAULT;
+        }
+
+        if (versionExpression.test(number) === false)
+        {
+            throw new InvalidVersionNumber(number);
         }
 
         const parts = number.split('.');
@@ -18,8 +30,7 @@ export default class VersionParser
         {
             case 1: return new Version(Number.parseInt(parts[0]));
             case 2: return new Version(Number.parseInt(parts[0]), Number.parseInt(parts[1]));
-            case 3: return new Version(Number.parseInt(parts[0]), Number.parseInt(parts[1]), Number.parseInt(parts[2]));
-            default: throw new InvalidVersionNumber(number);
+            default: return new Version(Number.parseInt(parts[0]), Number.parseInt(parts[1]), Number.parseInt(parts[2]));
         }
     }
 }
