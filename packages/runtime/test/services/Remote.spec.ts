efa4f524cf8aa2bdfbb99bab03f510a5c1e73161
Bas Meeuwissen <bas@masking.tech>
2024-01-29 19:30:46 +0100

    #451: implemented tests for remote

---
+++ packages/runtime/test/services/Remote.spec.ts
@@ -0,0 +1,47 @@
+
+import { describe, expect, it } from 'vitest';
+
+import Remote from '../../src/services/Remote';
+
+import { REQUESTS } from '../_fixtures/services/Remote.fixture';
+
+const remote = new Remote('http://localhost:3000');
+const CONTENT_TYPE = 'Content-Type';
+
+describe('services/Remote', () =>
+{
+    describe('.run', () =>
+    {
+        it('should return the response as boolean', async() =>
+        {
+            const response = await remote.run(REQUESTS.BOOLEAN_REQUEST);
+
+            expect(response.result).toBe(false);
+            expect(response.getHeader(CONTENT_TYPE)).toBe('application/boolean');
+        });
+
+        it('should return the response as number', async() =>
+        {
+            const response = await remote.run(REQUESTS.NUMBER_REQUEST);
+
+            expect(response.result).toBe(42);
+            expect(response.getHeader(CONTENT_TYPE)).toBe('application/number');
+        });
+
+        it('should return the response as object', async() =>
+        {
+            const response = await remote.run(REQUESTS.OBJECT_REQUEST);
+
+            expect(response.result).toEqual({ result: 42 });
+            expect(response.getHeader(CONTENT_TYPE)).toBe('application/json');
+        });
+
+        it('should return the response as string', async() =>
+        {
+            const response = await remote.run(REQUESTS.DEFAULT_REQUEST);
+
+            expect(response.result).toBe('Sorry, try again');
+            expect(response.getHeader(CONTENT_TYPE)).toBe('text/plain');
+        });
+    });
+});
