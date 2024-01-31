72af784816ffcd943a7843a3b28587cffc28b32f
Bas Meeuwissen <bas@masking.tech>
2024-01-31 06:30:48 +0100

    #450: replace env variables with values

--- packages/server-nodejs/src/utils/RuntimeConfigurationLoader.ts
+++ packages/server-nodejs/src/utils/RuntimeConfigurationLoader.ts
@@ -3,15 +3,35 @@ import { readFileSync } from 'fs';
 
 import RuntimeConfiguration, { runtimeSchema } from '../configuration/RuntimeConfiguration.js';
 
+import EnvironmentVariableNotFound from '../errors/EnvironmentVariableNotFound.js';
+
 import DataConverter from './DataConverter.js';
+
+const ENVIRONMENT_VARIABLE_REGEX = /\${([^}]*)}/g;
 
 export default class RuntimeConfigurationLoader
 {
     static load(filename: string): RuntimeConfiguration
     {
         const plainContents = readFileSync(filename, 'utf-8');
-        const parsedContents = JSON.parse(plainContents);
+        const replacedContents = this.#replaceEnvValues(plainContents);
+        const parsedContents = JSON.parse(replacedContents);
 
         return DataConverter.convert<RuntimeConfiguration>(runtimeSchema, parsedContents);
+    }
+
+    static #replaceEnvValues(contents: string): string
+    {
+        return contents.replace(ENVIRONMENT_VARIABLE_REGEX, (match, group) => 
+        {
+            const value = process.env[group];
+            
+            if (value === undefined)
+            {
+                throw new EnvironmentVariableNotFound(group);
+            }
+
+            return value;
+        });
     }
 }
