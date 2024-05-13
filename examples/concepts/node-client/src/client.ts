0a938f044ecbb5b4336f6d93d4b6a6836aeb5bb5
Bas Meeuwissen <bas@masking.tech>
2024-05-13 20:25:41 +0200

    #296: fixed node-client example

--- examples/concepts/node-client/src/client.ts
+++ examples/concepts/node-client/src/client.ts
@@ -1,9 +1,11 @@
 
-import { startClient } from 'jitar';
+import { startClient, Import } from 'jitar';
 
 const client = await startClient('http://127.0.0.1:3000', ['client']);
 
-const { default: getLuckyNumber } = await client.import('./getLuckyNumber', 'application') as any;
+const importModel = new Import('client', './getLuckyNumber', 'application');
+
+const { default: getLuckyNumber } = await client.import(importModel) as any;
 
 const number = await getLuckyNumber(0, 100);
 
