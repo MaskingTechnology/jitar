4da27f332f34d0add7d33aea4cdee6cad6286b19
Peter van Vliet <peter@masking.tech>
2024-08-27 13:02:03 +0200

    #296: fist working full-stack setup

--- examples/apps/full-stack/vite.config.ts
+++ examples/apps/full-stack/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import jitar from '@jitar/plugin-vite';
 
 export default defineConfig({
+  build: {
+    emptyOutDir: false
+  },
   plugins: [
     react(),
     jitar('src', 'domain', 'http://localhost:3000')
