08c23ef1865c303f15022526ab749a73e819f6fe
Peter van Vliet <peter@masking.tech>
2024-09-20 15:45:55 +0200

    #296: removed obsolete implementation

--- packages/middleware/src/index.ts
+++ packages/middleware/src/index.ts
@@ -1,8 +1,6 @@
 
 export { default as Middleware } from './interfaces/Middleware';
 
-export { default as ProcedureRunner } from './implementations/ProcedureRunner';
-
 export { default as NextHandler } from './types/NextHandler';
 
 export { default as MiddlewareManager } from './MiddlewareManager';
