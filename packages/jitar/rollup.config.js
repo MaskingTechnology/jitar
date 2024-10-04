cb82f7e31ef4015e0d569a70f1e990859dc0f870
Bas Meeuwissen <bas@masking.tech>
2024-10-05 00:16:49 +0200

    #552: added d.ts files for jitar

--- packages/jitar/rollup.config.js
+++ packages/jitar/rollup.config.js
@@ -1,12 +1,12 @@
 
-import terser from '@rollup/plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
 import { SERVER_EXTERNALS } from './rollup.definitions.js';
 
-function bundle(input, output, supportBrowser)
-{
+function bundle(input, output, supportBrowser) {
 	return {
 		external: SERVER_EXTERNALS,
 		treeshake: {
@@ -28,10 +28,19 @@ function bundle(input, output, supportBrowser)
 				browser: supportBrowser
 			})
 		]
+	}
+}
+
+function type(input, output) {
+	return {
+		input,
+		output: [{ file: output, format: 'module' }],
+		plugins: [dts({ respectExternal: true })],
 	};
 }
 
 export default [
 	bundle(['src/cli.ts', 'src/lib.ts'], { dir: 'dist' }, false),
-	bundle('src/client.ts', { file: 'dist/client.js' }, true)
+	bundle('src/client.ts', { file: 'dist/client.js' }, true),
+	type('./dist/types/lib.d.ts', 'dist/lib.d.ts')
 ];
