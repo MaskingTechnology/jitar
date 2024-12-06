b03292581b6e1c7dca6324cae261b076acd9497b
Bas Meeuwissen <bas@masking.tech>
2024-12-06 18:27:34 +0100

    #549: Constructor not required for static regexps

--- packages/logging/test/fixtures/values.fixture.ts
+++ packages/logging/test/fixtures/values.fixture.ts
@@ -29,22 +29,22 @@ export const INPUT =
 
 export const OUTPUT =
 {
-    DEBUG_CATEGORY: new RegExp('\\[DEBUG\\]\\[.*\\] debug'),
-    INFO_CATEGORY: new RegExp('\\[INFO\\]\\[.*\\] info'),
-    WARN_CATEGORY: new RegExp('\\[WARN\\]\\[.*\\] warn'),
-    ERROR_CATEGORY: new RegExp('\\[ERROR\\]\\[.*\\] error'),
-    FATAL_CATEGORY: new RegExp('\\[FATAL\\]\\[.*\\] fatal'),
+    DEBUG_CATEGORY: /\[DEBUG\]\[.*\] debug/,
+    INFO_CATEGORY: /\[INFO\]\[.*\] info/,
+    WARN_CATEGORY: /\[WARN\]\[.*\] warn/,
+    ERROR_CATEGORY: /\[ERROR\]\[.*\] error/,
+    FATAL_CATEGORY: /\[FATAL\]\[.*\] fatal/,
 
-    STRING: new RegExp('\\[.*\\]\\[.*\\] value'),
-    NUMBER: new RegExp('\\[.*\\]\\[.*\\] 1'),
-    BOOLEAN: new RegExp('\\[.*\\]\\[.*\\] true'),
-    OBJECT: new RegExp('\\[.*\\]\\[.*\\] {"key":"value"}'),
-    ARRAY: new RegExp('\\[.*\\]\\[.*\\] \\[\\n {2}value1,\\n {2}value2\\n\\]'),
-    FUNCTION: new RegExp('\\[.*\\]\\[.*\\] function'),
-    UNDEFINED: new RegExp('\\[.*\\]\\[.*\\] undefined'),
-    NULL: new RegExp('\\[.*\\]\\[.*\\] null'),
-    NESTED_OBJECT: new RegExp('\\[.*\\]\\[.*\\] {"key":{"key":{"key":"value"}}}'),
-    NESTED_ARRAY: new RegExp('\\[.*\\]\\[.*\\] \\[\\n {2}value,\\n {2}\\[\\n {4}value,\\n {4}\\[\\n {6}value\\n {4}\\]\\n {2}\\]\\n\\]'),
-    ERROR_WITH_STACKTRACE: new RegExp('\\[.*\\]\\[.*\\] Stacktrace'),
-    ERROR_WITHOUT_STACKTRACE: new RegExp('\\[.*\\]\\[.*\\] error without stacktrace')
+    STRING: /\[.*\]\[.*\] value/,
+    NUMBER: /\[.*\]\[.*\] 1/,
+    BOOLEAN: /\[.*\]\[.*\] true/,
+    OBJECT: /\[.*\]\[.*\] {"key":"value"}/,
+    ARRAY: /\[.*\]\[.*\] \[\n {2}value1,\n {2}value2\n\]/,
+    FUNCTION: /\[.*\]\[.*\] function/,
+    UNDEFINED: /\[.*\]\[.*\] undefined/,
+    NULL: /\[.*\]\[.*\] null/,
+    NESTED_OBJECT: /\[.*\]\[.*\] {"key":{"key":{"key":"value"}}}/,
+    NESTED_ARRAY: /\[.*\]\[.*\] \[\n {2}value,\n {2}\[\n {4}value,\n {4}\[\n {6}value\n {4}\]\n {2}\]\n\]/,
+    ERROR_WITH_STACKTRACE: /\[.*\]\[.*\] Stacktrace/,
+    ERROR_WITHOUT_STACKTRACE: /\[.*\]\[.*\] error without stacktrace/
 };
