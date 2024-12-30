bead38ae6144b0bac437c4127f5db2b199e038ed
Bas Meeuwissen <bas@masking.tech>
2024-12-30 15:40:02 +0100

    #585: type needed in the HttpRemote

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -3,6 +3,7 @@ export { default as Client } from './client/Client';
 export { default as ClientBuilder } from './client/ClientBuilder';
 
 export { default as ContentTypes } from './server/definitions/ContentTypes';
+export { default as AddWorkerRequest } from './server/types/AddWorkerRequest';
 export { default as ServerResponse } from './server/types/ServerResponse';
 export { default as Server } from './server/Server';
 export { default as ServerBuilder } from './server/ServerBuilder';
