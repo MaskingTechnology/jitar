501a030c5d3ac0ff2f32686fd9f6b1d64df9a99a
Bas Meeuwissen <bas@masking.tech>
2024-12-30 15:42:59 +0100

    #585: added explanation for implementation

--- packages/execution/src/errors/ProcedureNotAccessible.ts
+++ packages/execution/src/errors/ProcedureNotAccessible.ts
@@ -3,6 +3,10 @@ import { Forbidden } from '@jitar/errors';
 
 export default class ProcedureNotAccessible extends Forbidden
 {
+    // The fqn and versionNumber are not added as private properties,
+    // because this error message is never (de)serialized. It's added
+    // to the globalThis object and thrown directly.
+
     constructor(fqn: string, versionNumber: string)
     {
         super(`Procedure '${fqn}' (v${versionNumber}) is not accessible`);
