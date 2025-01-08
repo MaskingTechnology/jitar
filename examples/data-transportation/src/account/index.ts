609b25eccc691c98421ec33a44a44022870603b7
Peter van Vliet <peter@masking.tech>
2025-01-08 15:44:21 +0100

    #573: added index files to data-transporation example

---
+++ examples/data-transportation/src/account/index.ts
@@ -0,0 +1,3 @@
+
+export { default as Account } from './Account';
+export { default as createAccount } from './createAccount';
---
+++ examples/data-transportation/src/helpdesk/index.ts
@@ -0,0 +1,3 @@
+
+export { default as Registration } from './Registration';
+export { default as register } from './register';
