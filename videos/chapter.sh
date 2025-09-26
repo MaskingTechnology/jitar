b5cf234f5471380300f1067bfd73b32a9c6c37af
Bas Meeuwissen <bas@masking.tech>
2025-09-26 14:32:34 +0200

    #690 initial setup

---
+++ videos/chapter.sh
@@ -0,0 +1,4 @@
+
+CONFIG=$1
+
+python3 pdf2vid/tools/generate.py --type "chapter" --config "${CONFIG}" --output "productions"
---
+++ videos/video.sh
@@ -0,0 +1,4 @@
+
+CONFIG=$1
+
+python3 pdf2vid/tools/generate.py --type "video" --config "${CONFIG}" --output "productions"
