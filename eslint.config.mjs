9eaa88185d36748fdd66bce86c80b1ba18c0c906
Bas Meeuwissen <bas@masking.tech>
2025-12-01 13:30:06 +0100

    chore: updated eslint config (#722)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,45 @@
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config'
 import tsparser from '@typescript-eslint/parser';
 import jitar from 'eslint-plugin-jitar';
 import sonarjs from 'eslint-plugin-sonarjs';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-    ignores: [
-        "**/dist/**/*",
-        "**/node_modules/**/*",
-        "**/coverage/**/*",
-        "**/templates/**/*",
-        "**/test/**/fixtures/**/*",
-        "*config*"
-    ],
-    extends: [
-        eslint.configs.recommended,
-        ...tseslint.configs.recommended,
-        ...tseslint.configs.stylistic,
-        sonarjs.configs.recommended,
-    ],
-    languageOptions: {
-        parser: tsparser
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+    sonarjs.configs.recommended,
+    {
+        ignores: [
+            "**/dist/**/*",
+            "**/node_modules/**/*",
+            "**/coverage/**/*",
+            "**/templates/**/*",
+            "**/test/**/fixtures/**/*",
+            "*config*"
+        ]
     },
-    plugins: {
-        'jitar': jitar
+    {
+        languageOptions: {
+            parser: tsparser
+        },
+        plugins: {
+            'jitar': jitar
+        },
     },
-    rules: {
-        "@typescript-eslint/no-unsafe-function-type": "off",
-        "@typescript-eslint/consistent-type-definitions": "off",
-        "semi": ["error", "always"],
-        "eol-last": ["error", "always"],
-        "brace-style": ["error", "allman", { "allowSingleLine": true }],
-        "jitar/empty-first-line": "error",
+    {
+        rules: {
+            "@typescript-eslint/no-unsafe-function-type": "off",
+            "@typescript-eslint/consistent-type-definitions": "off",
+            "semi": ["error", "always"],
+            "eol-last": ["error", "always"],
+            "brace-style": ["error", "allman", { "allowSingleLine": true }],
+            "jitar/empty-first-line": "error",
 
-        "sonarjs/todo-tag": "off",
-        "sonarjs/slow-regex": "off",
-        "sonarjs/duplicates-in-character-class": "off"
+            "sonarjs/todo-tag": "off",
+            "sonarjs/slow-regex": "off",
+            "sonarjs/duplicates-in-character-class": "off"
+        }
     }
-});
+);
