f00e383464a2f894f55b55f927e15530c2cc72a3
Peter van Vliet <peter@masking.tech>
2026-05-08 12:32:54 +0200

    #767: refactored segment generation

--- packages/build/src/target/segment/Builder.ts
+++ packages/build/src/target/segment/Builder.ts
@@ -1,23 +1,16 @@
 
-import { ESFunction, ESIdentifierBinding, ESParameter } from '@jitar/analysis';
-import { ESArrayBinding, ESObjectBinding } from '@jitar/analysis';
-import { VersionParser } from '@jitar/execution';
 import { Logger } from '@jitar/logging';
 import type { FileManager } from '@jitar/sourcing';
 
-import { Keywords } from '../../definitions';
-import type { Application, Segment, SegmentModule } from '../../source';
-import { FileHelper } from '../../utils';
+import type { Application, Segment } from '../../source';
 
-const RUNTIME_IMPORTS = 'import { Segment, Class, Procedure, Implementation, Version, NamedParameter, ArrayParameter, ObjectParameter } from "jitar";';
+import Generator from './Generator';
 
 export default class Builder
 {
     readonly #targetFileManager: FileManager;
 
     readonly #logger: Logger;
-    readonly #fileHelper = new FileHelper();
-    readonly #versionParser = new VersionParser();
 
     constructor(targetFileManager: FileManager, logger: Logger)
     {
@@ -38,169 +31,11 @@ export default class Builder
     {
         const filename = `${segment.name}.segment.js`;
 
-        const code = this.#createCode(segment);
+        const generator = new Generator(segment);
+        const code = generator.generate();
 
         await this.#targetFileManager.write(filename, code);
 
         this.#logger.info(`Built ${segment.name} segment (${segment.modules.length} modules, ${segment.procedures.length} procedures, ${segment.classes.length} classes)`);
-    }
-
-    #createCode(segment: Segment): string
-    {
-        const importCode = this.#createImportCode(segment);
-        const segmentCode = this.#createSegmentCode(segment);
-
-        return `${importCode}\n${segmentCode}`;
-    }
-
-    #createImportCode(segment: Segment): string
-    {
-        const moduleImports = this.#createModuleImports(segment);
-
-        return `${RUNTIME_IMPORTS}\n${moduleImports}`;
-    }
-
-    #createModuleImports(segment: Segment): string
-    {
-        const imports = [];
-
-        // We only want to include modules that are defined in the segment configuration.
-        // The other modules contain classes and procedures that are re-exported by at least one segmented module.
-        // Adding them would cause a duplicate declaration error.
-
-        for (const module of segment.getSegmentedModules())
-        {
-            const filename = this.#fileHelper.addSubExtension(module.filename, segment.name);
-
-            if (module.members.length === 0)
-            {
-                const importRule = `import "./${filename}";`;
-
-                imports.push(importRule);
-                
-                continue;
-            }
-            
-            const members = this.#createModuleImportMembers(module);
-
-            const importRule = `import ${members} from "./${filename}";`;
-
-            imports.push(importRule);
-        }
-
-        return imports.join('\n');
-    }
-
-    #createModuleImportMembers(module: SegmentModule): string
-    {
-        const members = module.members;
-
-        const defaultImplementation = members.find(member => member.importKey === Keywords.DEFAULT);
-        const hasDefaultImplementation = defaultImplementation !== undefined;
-        const defaultMemberImport = hasDefaultImplementation ? defaultImplementation.id : '';
-
-        const namedImplementations = members.filter(member => member.importKey !== Keywords.DEFAULT);
-        const nameImplementationImports = namedImplementations.map(member => `${member.importKey} as ${member.id}`);
-        const hasNamedImplementations = namedImplementations.length > 0;
-        const groupedNamedMemberImports = hasNamedImplementations ? `{ ${nameImplementationImports.join(', ')} }` : '';
-
-        const separator = hasDefaultImplementation && hasNamedImplementations ? ', ' : '';
-
-        return `${defaultMemberImport}${separator}${groupedNamedMemberImports}`;
-    }
-
-    #createSegmentCode(segment: Segment): string
-    {
-        const lines: string[] = [];
-
-        lines.push(`export default new Segment("${segment.name}")`);
-
-        for (const clazz of segment.classes)
-        {
-            lines.push(`\t.addClass(new Class("${clazz.fqn}", ${clazz.id}))`);
-        }
-
-        for (const procedure of segment.procedures)
-        {
-            lines.push(`\t.addProcedure(new Procedure("${procedure.fqn}")`);
-
-            for (const implementation of procedure.implementations)
-            {
-                const version = this.#createVersionCode(implementation.version);
-                const parameters = this.#createParametersCode(implementation.model);
-
-                lines.push(`\t\t.addImplementation(new Implementation(${version}, "${implementation.access}", ${parameters}, ${implementation.id}))`);
-            }
-
-            lines.push('\t)');
-        }
-
-        return lines.join('\n');
-    }
-
-    #createVersionCode(versionString: string): string
-    {
-        const version = this.#versionParser.parse(versionString);
-
-        return `new Version(${version.major}, ${version.minor}, ${version.patch})`;
-    }
-
-    #createParametersCode(model: ESFunction): string
-    {
-        const result = this.#extractParameters(model.parameters);
-
-        return `[${result.join(', ')}]`;
-    }
-
-    #extractParameters(parameters: ESParameter[]): string[]
-    {
-        const result = [];
-
-        // Named parameters are identified by their name.
-        // Destructured parameters are identified by their index.
-
-        for (const parameter of parameters)
-        {
-            result.push(this.#extractParameter(parameter));
-        }
-
-        return result;
-    }
-
-    #extractParameter(parameter: ESParameter): string
-    {
-        if (parameter.binding instanceof ESArrayBinding)
-        {
-            return this.#createArrayParameter(parameter);
-        }
-        else if (parameter.binding instanceof ESObjectBinding)
-        {
-            return this.#createObjectParameter(parameter);
-        }
-
-        return this.#createNamedParameter(parameter);
-    }
-
-    #createNamedParameter(parameter: ESParameter): string
-    {
-        const binding = parameter.binding as ESIdentifierBinding;
-
-        return `new NamedParameter("${binding}", ${parameter.initializer !== undefined})`;
-    }
-
-    #createArrayParameter(parameter: ESParameter): string
-    {
-        const binding = parameter.binding as ESArrayBinding;
-        const members = this.#extractParameters(binding.elements);
-
-        return `new ArrayParameter([${members.join(', ')}])`;
-    }
-
-    #createObjectParameter(parameter: ESParameter): string
-    {
-        const binding = parameter.binding as ESArrayBinding;
-        const members = this.#extractParameters(binding.elements);
-
-        return `new ObjectParameter([${members.join(', ')}])`;
     }
 }
