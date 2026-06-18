44789ecfda680e9d347d59fb9dbbdbc6725bfffd
Peter van Vliet <peter@masking.tech>
2026-06-18 10:45:57 +0200

    #768: updated tests

--- packages/configuration/test/runtime/fixtures/files.fixture.ts
+++ packages/configuration/test/runtime/fixtures/files.fixture.ts
@@ -8,5 +8,6 @@ export const FILES: Record<string, File> =
 {
     DEFAULT: new File(FILENAMES.DEFAULT, 'text/json', JSON.stringify(CONFIGURATIONS.INPUT.DEFAULT)),
     VALID: new File(FILENAMES.VALID, 'text/json', JSON.stringify(CONFIGURATIONS.INPUT.RUNTIME)),
+    BUILD: new File(FILENAMES.BUILD, 'text/json', JSON.stringify(CONFIGURATIONS.INPUT.BUILD)),
     INVALID: new File(FILENAMES.INVALID, 'text/json', JSON.stringify(CONFIGURATIONS.INPUT.INVALID))
 } as const;
